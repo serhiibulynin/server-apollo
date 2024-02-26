e1cf00ff2de38216bf0e9724fafbdd885ffaf09c
serhiibulynin <81964924+serhiibulynin@users.noreply.github.com>
2024-02-26 18:03:57 +0200

    fix sequilize

---
+++ database/config.mjs
@@ -0,0 +1,23 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export default {
+  development: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    seederStorage: 'sequelize',
+  },
+  production: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    seederStorage: 'sequelize',
+  },
+};
